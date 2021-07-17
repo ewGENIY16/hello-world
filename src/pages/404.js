aacbda9f9e8dfe553af1e631a3c65fc1dfc6c0b4
ewGENIY16 <85677491+ewGENIY16@users.noreply.github.com>
2021-07-17 15:41:55 +0300

    master

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound() {
+    return (
+        <div>
+            <h2>404</h2>
+            <p>Извените, на данный момент страница не существует.</p>
+        </div>
+    )
+}
