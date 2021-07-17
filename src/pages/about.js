bac8a1308b4846a99dd4d5ca46b55e4270d22d59
ewGENIY16 <85677491+ewGENIY16@users.noreply.github.com>
2021-07-17 14:48:59 +0300

    master

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <h1>About page</h1>
+            <p>Текст, текст, текст, текст</p>
+            <p>Текст, текст, текст, текст</p>
+            <p>Текст, текст, текст, текст</p>   
+        </div>
+    )
+}
