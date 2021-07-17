de04fb51f7b1db9c2d05a260502a542719c405d5
ewGENIY16 <85677491+ewGENIY16@users.noreply.github.com>
2021-07-17 15:34:47 +0300

    master

---
+++ src/pages/projects/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Projects() {
+    return (
+        <div>
+            <h2>Портфолио</h2>
+            <h3>Проекты и созданные сайты</h3>
+        </div>
+    )
+}
