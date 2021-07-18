080b09167ddb6e53eaf97aa8ac30e7d1e7c9f9b3
ewGENIY16 <85677491+ewGENIY16@users.noreply.github.com>
2021-07-18 15:50:40 +0300

    master

---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h1>Web Warrior</h1>
+            <div className="links">
+            
+        </nav>
+    )
+}
