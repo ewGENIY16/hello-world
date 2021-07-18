2d285ad2a804581f9992a49b3a26bee9b88d4d4f
ewGENIY16 <85677491+ewGENIY16@users.noreply.github.com>
2021-07-18 18:24:24 +0300

    master

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
+import Navbar from "../components/navbar"
 
 export default function Home() {
   return (
     <section>
+      <Navbar />
       <div>
         <h2>design</h2>
         <h3>Develop & Deploy</h3>
